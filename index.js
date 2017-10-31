05a28223c38d4307c7dab428db7bc544c459a994
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-10-31 03:03:53 +0100

    npm

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var OSinfo = require('../modules/OSinfo');
+var OSinfo = require('./modules/OSinfo');
 
 process.stdin.setEncoding('utf-8');
 
-console.log('Your NodeJS version: ' + process.version);
-console.log('Your system: ' + process.env.OS);
 process.stdin.on('readable', function() {
     // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,5 +1,6 @@
 var os = require('os');
 var timeFormat = require('./timeFormat');
+var colors = require('colors');
 
 function getOSinfo() {
     var type = os.type();
@@ -13,13 +14,12 @@ function getOSinfo() {
     var uptime = timeFormat.print(os.uptime());
     var userInfo = os.userInfo();
 
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    //console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('Uptime:', uptime);
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('System:'.gray.bold, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
+    console.log('Uptime:'.green, uptime);
+    console.log('User name:'.yellow, userInfo.username);
+    console.log('Home dir:'.grey, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
